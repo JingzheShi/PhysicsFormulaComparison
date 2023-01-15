5bd238fe056b1ed92133d7dd56899c5f21fa2336
XIAOYU XIONG <xxyfh2@outlook.com>
2023-01-15 11:07:57 +0800

    Multi-processing

--- Formula_Compare/student_answer.py
+++ Formula_Compare/student_answer.py
@@ -1,9 +1,23 @@
 from problem_answer import ProblemAnswer
+from problem_answer import build_Problem_Answer
+from problem_answer import Node
 class StudentAnswer_for_SingleProblem():
-    def __init__(self,studentID,problemID,latexList):
+    def __init__(self,studentID,problemName,problemID,latexList):
         self.studentId = studentID
         self.problemID = problemID
+        self.problemName = problemName
         self.studentLatexLst = latexList
         self.points = -1
+        self.studentScoreDct = dict()
+        problem_answer = build_Problem_Answer(problemID,problemName)
+        def build_dct(node:Node):
+            if node.ChildrenNodeType == "formula":
+                for child in node.children_lst:
+                    self.studentScoreDct[child.TokenStr] = 0
+            else:
+                for child_node in node.children_lst:
+                    build_dct(child_node)
+        build_dct(problem_answer.root_node)
+        
         
         
