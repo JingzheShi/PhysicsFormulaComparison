df750597ff707b22193f9b52facde89e7d24847f
XIAOYU XIONG <xxyfh2@outlook.com>
2023-01-19 12:25:31 +0800

    Add multiprocessing into answer list generation

--- utils/multi_students_single_problem_scoring_utils.py
+++ utils/multi_students_single_problem_scoring_utils.py
@@ -30,17 +30,29 @@ def compare_problemFormula_with_studentAnswer(dct):
 def multiStudents_compare_oneProblem(problemFormulas:ProblemFormulas,studentsAnswersAndScores:dict,N_process:int):
     #studentsAnswersAndScores: dict, key is studentID, value is of type Student_AnswersAndScores_for_SingleProblem
     formulas_dct = problemFormulas.Formula_Dct
+    parsing_dcts_lst_root = []
     parsing_dcts_lst = []
     
     for studentID,studentAnswersAndScores in studentsAnswersAndScores.items():
+        parsing_dcts_lst_root.append([studentID])
+
+    def generate_answer_compare_lst(input_lst):
+        studentID = input_lst[0]
         for formulaToken, formula in formulas_dct.items():
-            for studentAnswerLatex in studentAnswersAndScores.studentLatexLst:
-                parsing_dcts_lst.append(dict(
+            for studentAnswerLatex in studentAnswersAndScores[studentID].studentLatexLst:
+                input_lst.append(dict(
                     studentID = studentID,
                     formulaToken = formulaToken,
                     problemFormula = formula,
                     studentAnswerLatex = studentAnswerLatex
                 ))
+
+    with multiprocessing.Pool(processes=N_process) as pool:
+        output_compare_lst = pool.map(generate_answer_compare_lst, parsing_dcts_lst_root)
+
+    for lst in output_compare_lst:
+        del(lst[0])
+        parsing_dcts_lst.extend(lst)
                 
     with multiprocessing.Pool(processes=N_process) as pool:
         output_lst = pool.map(compare_problemFormula_with_studentAnswer, parsing_dcts_lst)
